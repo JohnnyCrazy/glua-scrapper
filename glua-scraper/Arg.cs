634925696f886f4823a878be9dc29fb993e6810c
Jonas Dellinger <johnny@johnnycrazy.de>
2016-09-30 01:06:21 +0200

    Added converting of default arg functions

--- glua-scraper/Arg.cs
+++ glua-scraper/Arg.cs
@@ -1,18 +1,37 @@
-﻿namespace glua_scraper
+﻿using System.Text.RegularExpressions;
+
+namespace glua_scraper
 {
     public class Arg
     {
-        public string Type { get; set; }
-        public string Name { get; set; }
-        public string Desc { get; set; }
-        public string Default { get; set; }
-
         public Arg(string raw)
         {
             Type = WikiArticle.GetValue(raw, "type");
             Name = WikiArticle.GetValue(raw, "name");
             Desc = WikiArticle.GetValue(raw, "desc");
-            Default = WikiArticle.GetValue(raw, "default");
+            Default = ConvertDefaultValue(WikiArticle.GetValue(raw, "default"));
+        }
+
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string Default { get; set; }
+
+        private string ConvertDefaultValue(string defaultValue)
+        {
+            Match m = Regex.Match(defaultValue, @"\{\{(.*)\|(.*)\}\}\(\)");
+            if (m.Success)
+            {
+                switch (m.Groups[1].Value)
+                {
+                    case "GlobalFunction":
+                        return $"{m.Groups[2].Value}()";
+                    // TODO Add more cases if necessary
+                    default:
+                        return defaultValue;
+                }
+            }
+            return defaultValue;
         }
     }
 }
